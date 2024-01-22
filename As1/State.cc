7607e9e869252d13e9d554a7358c8920f2991a62
KeaganRieder <keagan@rieder.ca>
2024-01-22 12:11:27 -0700

    figured out how to represent stats, started writing bfs

--- As1/State.cc
+++ As1/State.cc
@@ -1,47 +1,47 @@
-#include <State.h>
+// #include <State.h>
 
-State::State(/* args */)
-{
-    //default which is nothing?
-}
-State::State(StateData leftSide, StateData rightSide, StateData boatSide)
-{
-    this->leftSide = leftSide;
-    this->rightSide = rightSide;
-    this->boat = boatSide;
-}
+// State::State(/* args */)
+// {
+//     //default which is nothing?
+// }
+// State::State(StateData leftSide, StateData rightSide, StateData boatSide)
+// {
+//     this->leftSide = leftSide;
+//     this->rightSide = rightSide;
+//     this->boat = boatSide;
+// }
 
-State::~State()
-{
-    //empty?
-}
+// State::~State()
+// {
+//     //empty?
+// }
  
-void State::SetLeftSide(StateData leftSide){
-    this->leftSide = leftSide;
-}
-StateData State::getLeftSide(){
-    return leftSide;
-}
-bool State::CheckLeftValid(){
-    return leftSide.missionaries >= leftSide.cannibals;
-}
+// void State::SetLeftSide(StateData leftSide){
+//     this->leftSide = leftSide;
+// }
+// StateData State::getLeftSide(){
+//     return leftSide;
+// }
+// bool State::CheckLeftValid(){
+//     return leftSide.missionaries >= leftSide.cannibals;
+// }
 
-void State::SetRightSide(StateData RightSide){
-    this->rightSide = rightSide;
-}
-StateData State::getRightSide(){
-    return rightSide;
-}
-bool State::CheckRightValid(){
-    return rightSide.missionaries >= rightSide.cannibals;
-}
+// void State::SetRightSide(StateData RightSide){
+//     this->rightSide = rightSide;
+// }
+// StateData State::getRightSide(){
+//     return rightSide;
+// }
+// bool State::CheckRightValid(){
+//     return rightSide.missionaries >= rightSide.cannibals;
+// }
     
-void State::SetBoat(StateData Boat){
-    this->boat = boat;
-}
-StateData State::getBoat(){
-    return boat;
-}
-bool State::CheckBoatValid(){
-    return boat.missionaries >= boat.cannibals;
-} 
+// void State::SetBoat(StateData Boat){
+//     this->boat = boat;
+// }
+// StateData State::getBoat(){
+//     return boat;
+// }
+// bool State::CheckBoatValid(){
+//     return boat.missionaries >= boat.cannibals;
+// } 
--- As1/State.h
+++ As1/State.h
@@ -1,44 +1,44 @@
-#ifndef STATE_H
-#define STATE_H
+// #ifndef STATE_H
+// #define STATE_H
 
-#include "SubProblem.h"
+// #include "SubProblem.h"
 
-using namespace std;
+// using namespace std;
 
-// class which handles that currnet state in the problem 
-class State
-{
-    private:
-    StateData leftSide;
-    StateData rightSide;
-    StateData boat;
-    //todo make parent state
+// // class which handles that currnet state in the problem 
+// class State
+// {
+//     private:
+//     StateData leftSide;
+//     StateData rightSide;
+//     StateData boat;
+//     //todo make parent state
         
-    public:
-    State();
-    State(StateData leftSide,StateData rightSide,StateData boatSide);
-    ~State();
+//     public:
+//     State();
+//     State(StateData leftSide,StateData rightSide,StateData boatSide);
+//     ~State();
 
-    //used to update or set the left side of the current state
-    void SetLeftSide(StateData leftSide);
-    //used to get the current left side of the current state
-    StateData getLeftSide();
-    //checks if LeftSide is valid
-    bool CheckLeftValid();
+//     //used to update or set the left side of the current state
+//     void SetLeftSide(StateData leftSide);
+//     //used to get the current left side of the current state
+//     StateData getLeftSide();
+//     //checks if LeftSide is valid
+//     bool CheckLeftValid();
 
-    //used to update or set the Right side of the current state
-    void SetRightSide(StateData RightSide);
-    //used to get the current Right side of the current state
-    StateData getRightSide();
-    //checks if rightSide is valid
-    bool CheckRightValid();
+//     //used to update or set the Right side of the current state
+//     void SetRightSide(StateData RightSide);
+//     //used to get the current Right side of the current state
+//     StateData getRightSide();
+//     //checks if rightSide is valid
+//     bool CheckRightValid();
 
     
-    //used to update or set the Boat of the current state
-    void SetBoat(StateData Boat);
-    //used to get the  Boat of the current state
-    StateData getBoat();
-    // checks if boat is valid
-    bool CheckBoatValid();  
-};
-#endif
+//     //used to update or set the Boat of the current state
+//     void SetBoat(StateData Boat);
+//     //used to get the  Boat of the current state
+//     StateData getBoat();
+//     // checks if boat is valid
+//     bool CheckBoatValid();  
+// };
+// #endif
